0374d972cdcbb6ac89ab2e3f74cf4b3d82c28e8d
Robert Vollmert <rfvollmert@gmail.com>
2015-01-20 12:34:30 +0100

    Pluggable error handling.

---
+++ errer.go
@@ -0,0 +1,29 @@
+package bubbles
+
+import (
+	"log"
+)
+
+// Errer handles errors and warnings.
+type Errer interface {
+	// Error handles a fatal error.
+	Error(error)
+
+	// Warning handles a non-fatal error.
+	Warning(error)
+}
+
+// DefaultErrer implements Errer, logging with log.
+type DefaultErrer struct{}
+
+// Error handles a fatal error.
+func (DefaultErrer) Error(err error) {
+	log.Print(err)
+}
+
+// Warning handles a non-fatal error.
+func (DefaultErrer) Warning(err error) {
+	log.Print(err)
+}
+
+var _ Errer = DefaultErrer{}
